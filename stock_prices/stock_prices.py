75948fc7804a49e2b78110d3a9ec7ad6b9423ca3
Connor Sullivan <crsullivan@protonmail.com>
2020-02-20 14:11:40 -0700

    bit stuck on stocks but everything looks ok to me... should work, gonna step away for a bit and come back to it

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,8 +3,18 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
-
+  current_min_price_so_far = 0
+  max_profit_so_far = 0
+  for i in range(len(prices)):
+    if prices[i] < current_min_price_so_far:
+      current_min_price_so_far = prices[i]
+      print(current_min_price_so_far)
+    for j in range(i + 1, len(prices)):
+      profit = prices[j] - prices[i]
+      if profit > max_profit_so_far:
+        max_profit_so_far = profit
+        print(max_profit_so_far)
+  return max_profit_so_far
 
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
@@ -13,3 +23,5 @@ if __name__ == '__main__':
   args = parser.parse_args()
 
   print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+
+  print(find_max_profit([1050, 270, 1540, 3800, 2]))
