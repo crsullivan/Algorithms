df92ef3292564b09811aac3a6dc85036b16bd91f
Connor Sullivan <crsullivan@protonmail.com>
2020-02-20 12:34:21 -0700

    recursion is key

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -6,20 +6,32 @@ import sys
 # a solution that is more efficient than the naive 
 # recursive solution
 def eating_cookies(n, cache=None):
+  print('first', n)
+  if n == 0:
+    return 1
+  elif n == 1:
+    return 1
+  elif n == 2:
+    return 2
+  elif n == 3:
+    return 4
+  else:
+    print('second', n)
+    return eating_cookies(n-3) + eating_cookies(n-2) + eating_cookies(n-1)
 
-  n_factorial = 1
-  nr_factorial = 1
-  for i in range(1, n+1):
-    n_factorial = n_factorial * i
+  # n_factorial = 1
+  # nr_factorial = 1
+  # for i in range(1, n+1):
+  #   n_factorial = n_factorial * i
 
-  for j in range(1, n):
-    nr_factorial = nr_factorial * j
+  # for j in range(1, n):
+  #   nr_factorial = nr_factorial * j
 
-  perm = n_factorial 
+  # perm = n_factorial 
 
-  return perm
+  # return perm
   
-
+print(eating_cookies(5))
 if __name__ == "__main__":
   if len(sys.argv) > 1:
     num_cookies = int(sys.argv[1])
