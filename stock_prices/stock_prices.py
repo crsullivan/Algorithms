93764839bad23a99f418b06703e7b8217da7c46c
Connor Sullivan <crsullivan@protonmail.com>
2020-03-19 15:04:58 -0700

    recursive stock prices

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,11 +3,27 @@ import math
 import argparse
 
 def find_max_profit(prices):
-  
-  pass
-  
+  # Give me another list of the same numbers sorted least to greatest
+  for i in range(len(prices)):
+    if prices.index(min(prices)) < prices.index(max(prices)):
+      return max(prices) - min(prices)
+    elif prices.index(max(prices)) == 0 and prices.index(min(prices)) == len(prices) - 1:
+      diff = [prices[i+1]-prices[i] for i in range(len(prices)-1)] 
+      return max(diff)
+    elif prices.index(min(prices)) == len(prices) - 1:
+      prices.remove(min(prices))
+      find_max_profit(prices)
+    elif prices.index(max(prices)) == 0:
+      prices.remove(max(prices))
+      find_max_profit(prices)
+    
 
-find_max_profit([1050, 270, 1540, 3800, 2])
+  
+# x = [100, 55, 4, 98, 10, 18, 90, 95, 43, 11, 47, 67, 89, 42, 49, 79]
+# y = [1, 55, 678, 3, 3214]
+# z = [1050, 270, 1540, 3800, 2]
+# v = [100, 90, 80, 50, 20, 10]
+# print(find_max_profit(v))
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
   parser = argparse.ArgumentParser(description='Find max profit from prices.')
